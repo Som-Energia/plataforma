9de8601c9853226146c6540983b9a2cf2b792321
David Romaní <david@flux.cat>
2013-06-19 16:00:19 +0200

    mod/file euskara translation

--- mod/file/languages/eu.php
+++ mod/file/languages/eu.php
@@ -1,76 +1,110 @@
 <?php
-$basque=array (
-  'file' => 'Fitxategiak',
-  'file:user' => '%s-(r)en fitxategiak',
-  'file:friends' => 'Lagunen fitxategiak',
-  'file:all' => 'Fitxategi guztiak',
-  'file:edit' => 'Editatu fitxategia',
-  'file:more' => 'Fitxategi gehiago',
-  'file:list' => 'zerrenda ikuspegia',
-  'file:group' => 'Taldeko fitxategiak',
-  'file:gallery' => 'galeria ikuspegia',
-  'file:gallery_list' => 'Galeria edo zerrenda ikuspegia',
-  'file:num_files' => 'Erakutsiko diren fitxategi kopurua',
-  'file:user:gallery' => 'Ikusi %s galeria',
-  'file:upload' => 'Fitxategi bat igo',
-  'file:replace' => 'Ordeztu fitxategi edukia (utzi hutsik fitxategia ez aldatzeko)',
-  'file:list:title' => '%s-(r)en %s %s',
-  'file:title:friends' => 'Lagunenak',
-  'file:add' => 'Fitxategi bat igo',
-  'file:file' => 'Fitxategia',
-  'file:title' => 'Izenburua',
-  'file:desc' => 'Deskribapena',
-  'file:tags' => 'Etiketak',
-  'file:list:list' => 'Aldatu zerrenda iksupegira',
-  'file:list:gallery' => 'Aldatu galeria ikuspegira',
-  'file:types' => 'Igotako fitxategi motak',
-  'file:type:' => 'Fitxategiak',
-  'file:type:all' => 'Fitxategi guztiak',
-  'file:type:video' => 'Bideoak',
-  'file:type:document' => 'Dokumentuak',
-  'file:type:audio' => 'Audioa',
-  'file:type:image' => 'Irudiak',
-  'file:type:general' => 'Orokorra',
-  'file:user:type:video' => '%s-(r)en bideoak',
-  'file:user:type:document' => '%s-(r)en dokumentuak',
-  'file:user:type:audio' => '%s-(r)en audio fitxatetiak',
-  'file:user:type:image' => '%s-(r)en irudiak',
-  'file:user:type:general' => '%s-(r)en fitxategi orokorrak',
-  'file:friends:type:video' => 'Zure lagunen bideoak',
-  'file:friends:type:document' => 'Zure lagunen dokumentuak',
-  'file:friends:type:audio' => 'Zure lagunen audioak',
-  'file:friends:type:image' => 'Zure lagunen irudiak',
-  'file:friends:type:general' => 'Zure lagunen fitxategi orokorrak',
-  'file:widget' => 'Fitxategi widgeta',
-  'file:widget:description' => 'Zure fitxategi berrien erakusleihoa',
-  'groups:enablefiles' => 'Baimendu talde fitxategiak',
-  'file:download' => 'Hau deskargatu',
-  'file:delete:confirm' => 'Ziur fitxategi hau ezabatu nahi duzula?',
-  'file:tagcloud' => 'Etiketa lainoa',
-  'file:display:number' => 'Erakutsi beharreko fitxategi kopurua',
-  'river:create:object:file' => '%s-(e)k %s fitxategia igo du',
-  'river:comment:object:file' => '%s-(e)k %s fitxategia iruzkindu du',
-  'item:object:file' => 'Fitxategiak',
-  'file:newupload' => 'Fitxategi berri bat igo da',
-  'file:notification' => '%s-ek fitxategi berria kargatu du:
+/**
+ * Elgg file plugin language pack
+ *
+ * @package ElggFile
+ */
 
-%s
-%s
+$euskara = array(
 
-Ikusi eta iruzkina egin fitxategi berriari:
-%s
-',
-  'file:embed' => 'Multimedia txertatu',
-  'file:embedall' => 'Guztia',
-  'file:saved' => 'Zure fitxategia ongi gorde da.',
-  'file:deleted' => 'Zure fitxategia ongi ezabatu da.',
-  'file:none' => 'Ez da fitxategirik.',
-  'file:uploadfailed' => 'Barkatu; ezin izan dugu zure fitxategia gorde.',
-  'file:downloadfailed' => 'Barkatu; fitxategi hau ez dago eskuragarri orain.',
-  'file:deletefailed' => 'Zure fitxategia ezin izan da ezabatu orain.',
-  'file:noaccess' => 'Ez duzu fitxategi hau aldatzeko baimeni',
-  'file:cannotload' => 'Fitxategia igotzean errore bat gertatu da',
-  'file:nofile' => 'Fitxategi bat hautatu behar duzu',
+    /**
+     * Menu items and titles
+     */
+    'file' => "Fitxategiak",
+    'file:user' => "%s(r)en fitxategiak",
+    'file:friends' => "Lagunen fitxategiak",
+    'file:all' => "Leku osoaren fitxategiak",
+    'file:edit' => "Editatu fitxategia",
+    'file:more' => "Fitxategi gehiago",
+    'file:list' => "zerrenda ikuspegia",
+    'file:group' => "Taldearen fitxategiak",
+    'file:gallery' => "galeria ikuspegia",
+    'file:gallery_list' => "Galeria edo zerrenda ikuspegia",
+    'file:num_files' => "Ageri beharreko fitxategi kopurua",
+    'file:user:gallery'=>'Ikusi %s galeria',
+    'file:via' => "fitxategien bidez",
+    'file:upload' => "Igo fitxategi bat",
+    'file:replace' => "Aldatu fitxategiaren edukia (utzi zuriz fitxategia ez aldatzeko)",
+    'file:list:title' => "%s(r)en %s %s",
+    'file:title:friends' => "Lagunena(k)",
+
+    'file:add' => "Igo fitxategi bat",
+
+    'file:file' => "Fitxategia",
+    'file:title' => "Izenburua",
+    'file:desc' => "Deskripzioa",
+    'file:tags' => "Etiketak",
+
+    'file:list:list' => "Aldatu zerrenda ikuspegira",
+    'file:list:gallery' => "Aldatu galeria ikuspegira",
+
+    'file:types' => "Igotako fitxategi motak",
+
+    'file:type:' => "Fitxategiak",
+    'file:type:all' => "Fitxategi guztiak",
+    'file:type:video' => "Bideoak",
+    'file:type:document' => "Dokumentuak",
+    'file:type:audio' => "Audioak",
+    'file:type:image' => "Irudiak",
+    'file:type:general' => "Orokorra",
+
+    'file:user:type:video' => "%s(r)en bideoak",
+    'file:user:type:document' => "%s(r)en dokumentuak",
+    'file:user:type:audio' => "%s(r)en audioak",
+    'file:user:type:image' => "%s(r)en irudiak",
+    'file:user:type:general' => "%s(r)en fitxategi orokorrak",
+
+    'file:friends:type:video' => "Zure lagunen bideoak",
+    'file:friends:type:document' => "Zure lagunen doumentuak",
+    'file:friends:type:audio' => "Zure lagunen audioak",
+    'file:friends:type:image' => "Zure lagunen irudiak",
+    'file:friends:type:general' => "Zure lagunen fitxategi orokorrak",
+
+    'file:widget' => "Widget fitxategia",
+    'file:widget:description' => "Erakutsi zure azken fitxategiak",
+
+    'groups:enablefiles' => "Prestatu talde fitxategiak",
+
+    'file:download' => "Deskargatu hau",
+
+    'file:delete:confirm' => "Ziur al zaude fitxategi hau ezabatu nahi duzula?",
+
+    'file:tagcloud' => "Etiketa hodeia",
+
+    'file:display:number' => "Ageri beharreko fitxategi kopurua",
+
+    'river:create:object:file' => "%s(e)k %s fitxategia igo du",
+    'river:comment:object:file' => "%s(e)k %s fitxategiari iruzkina egin dio",
+
+    'item:object:file' => "Fitxategiak",
+
+    'file:newupload' => "Fitxategi berria igo dute",
+
+    /**
+     * Embed media
+     **/
+
+    'file:embed' => "Media txertatua",
+    'file:embedall' => "Guztia",
+
+    /**
+     * Status messages
+     */
+
+    'file:saved' => "Zure fitxategia egoki gorde da.",
+    'file:deleted' => "Zure fitxategia egoki ezabatu da.",
+
+    /**
+     * Error messages
+     */
+
+    'file:none' => "Fitxategirik ez.",
+    'file:uploadfailed' => "Barkatu; ezin izan dugu zure fitxategia gorde.",
+    'file:downloadfailed' => "Barkatu; fitxategi hau ez dago erabilgarri oraintxe.",
+    'file:deletefailed' => "Ezin izan da zure fitxategia oraintxe ezabatu.",
+    'file:noaccess' => "Ez duzu fitxategi hau aldatzeko baimenik",
+    'file:cannotload' => "Errorea egon da fitxategi hau igotzean",
+    'file:nofile' => "Fitxatgi bat hautatu behar duzu"
 );
 
-add_translation("eu", $basque);
+add_translation('eu', $euskara); 
