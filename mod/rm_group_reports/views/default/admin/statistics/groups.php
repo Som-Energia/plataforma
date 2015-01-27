a72b48e0870caff129489600b7c1cfa88910dc08
Rubén Martín <yo@rubenmartin.me>
2015-01-27 13:01:18 +0000

    Plugin — rm_group_reports: Añadir el cursor Pointer a la cabeza de la tabla y incorporar estilo de botón a "eliminar grupo"

--- mod/rm_group_reports/views/default/admin/statistics/groups.php
+++ mod/rm_group_reports/views/default/admin/statistics/groups.php
@@ -29,13 +29,13 @@ echo '<table class="sortable elgg-table-alt">';
 
 echo <<<THEAD
     <thead style="background-color: #eee;">
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Miembros</th>
-            <th>Creado</th>
-            <th>Última actividad</th>
-            <th></td>
+        <tr style="cursor: pointer;">
+            <th style="padding: 10px 0">ID</th>
+            <th style="padding: 10px 0">Nombre</th>
+            <th style="padding: 10px 0">Miembros</th>
+            <th style="padding: 10px 0">Creado</th>
+            <th style="padding: 10px 0">Última actividad</th>
+            <th style="padding: 10px 0"></td>
         </tr>
     </thead>
 THEAD;
@@ -46,21 +46,22 @@ foreach( $table as $activity => $row ) {
     
     echo '<tr>';
     
-    echo '<td>' . $row->get( 'guid' ) . '</td>';
-    echo '<td><a href="' . $row->getURL() . '" target="_blank">' . $row->get( 'name' ) . '</a></td>';
-    echo '<td><a href="' . elgg_get_config( 'url' ) . 'groups/members/' . $row->get( 'guid' ) . '" target="_blank">' . $row->getMembers( 0, 0, true ) . '</a></td>';
-    echo '<td sorttable_customkey="' . $row->get( 'time_created' ) . '">' . date( 'd/m/Y H:i', $row->get( 'time_created' ) ) . '</td>';
+    echo '<td style="padding: 10px 0">' . $row->get( 'guid' ) . '</td>';
+    echo '<td style="padding: 10px 0"><a href="' . $row->getURL() . '" target="_blank">' . $row->get( 'name' ) . '</a></td>';
+    echo '<td style="padding: 10px 0"><a href="' . elgg_get_config( 'url' ) . 'groups/members/' . $row->get( 'guid' ) . '" target="_blank">' . $row->getMembers( 0, 0, true ) . '</a></td>';
+    echo '<td style="padding: 10px 0" sorttable_customkey="' . $row->get( 'time_created' ) . '">' . date( 'd/m/Y H:i', $row->get( 'time_created' ) ) . '</td>';
     
     if ( $activity != $row->get( 'time_created' ) )
-        echo '<td sorttable_customkey="' . $activity . '">' . date( 'd/m/Y H:i', $activity ) . '</td>';
+        echo '<td style="padding: 10px 0" sorttable_customkey="' . $activity . '">' . date( 'd/m/Y H:i', $activity ) . '</td>';
     else
-        echo '<td sorttable_customkey="' . $row->get( 'time_created' ) . '">&#8212;</td>';
+        echo '<td style="padding: 10px 0" sorttable_customkey="' . $row->get( 'time_created' ) . '">&#8212;</td>';
     
-    echo '<td>';
+    echo '<td style="padding: 10px 0">';
     echo elgg_view('output/confirmlink', array(
-        'text' => elgg_echo('groups:delete'),
-        'href' => 'action/groups/delete?guid=' . $row->getGUID(),
-        'confirm' => elgg_echo('groups:deletewarning'),
+        'text'      => elgg_echo('groups:delete'),
+        'href'      => 'action/groups/delete?guid=' . $row->getGUID(),
+        'confirm'   => elgg_echo('groups:deletewarning'),
+        'class'     => array( 'elgg-button', 'elgg-button-action' ),
     ));
     echo '</td>';
     
