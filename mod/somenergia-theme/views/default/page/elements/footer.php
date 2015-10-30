4d88144eca63207d2c3d5db8fcb8a77c8af86b7f
Blackleg <blackleg@openaliasbox.org>
2015-10-30 23:00:03 +0100

    Añadido footer al tema

---
+++ mod/somenergia-theme/views/default/page/elements/footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Elgg footer
+ * The standard HTML footer that displays across the site
+ *
+ * @package Elgg
+ * @subpackage Core
+ *
+ */
+
+echo elgg_view_menu('footer', array('sort_by' => 'priority', 'class' => 'elgg-menu-hz'));
+
+
+echo '<div class="mts clearfloat float-alt">';
+echo elgg_view('output/url', array(
+	'href' => 'http://elgg.org',
+	'text' => "<img src=\"$powered_url\" alt=\"Powered by Elgg\" width=\"106\" height=\"15\" />",
+	'class' => '',
+	'is_trusted' => true,
+));
+echo '</div>';
