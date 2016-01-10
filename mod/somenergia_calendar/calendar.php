c0372eb4e2b45fcd6cf8a91a377c87232e69c7be
Blackleg <blackleg@openaliasboox.org>
2016-01-10 16:20:24 +0100

    Soporte CSS en el plugin somenergia calendar

--- mod/somenergia_calendar/calendar.php
+++ mod/somenergia_calendar/calendar.php
@@ -10,7 +10,7 @@
 
 $title = elgg_echo('item:object:somenergia_calendar');
 
-$content = '<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=73q91crmmthkfqi1jgtj02flks%40group.calendar.google.com&amp;color=%23333333&amp;ctz=Europe%2FMadrid" style=" border-width:0 " scrolling="no" frameborder="0" height="600" width="100%"></iframe>';
+$content = '<iframe class="somenergiacalendar"  src="https://www.google.com/calendar/embed?showTitle=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=73q91crmmthkfqi1jgtj02flks%40group.calendar.google.com&amp;color=%23333333&amp;ctz=Europe%2FMadrid"></iframe>';
 
 
 $body = elgg_view_layout('content', array(
---
+++ mod/somenergia_calendar/views/default/somenergia_calendar/css.php
@@ -0,0 +1,8 @@
+
+.somenergiacalendar {
+        padding: 0.5em;
+        width: 100%;
+        height: 40em;
+        
+}
+
