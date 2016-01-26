f06b7ec9d33ec7d44ba7368308598a7f69f053ab
Francesc <francesccasadellaoller@gmail.com>
2016-01-26 17:12:32 +0100

    Traducción somenergiaheader català

--- mod/somenergia-theme/languages/es.php
+++ mod/somenergia-theme/languages/es.php
@@ -7,7 +7,7 @@
 
 $spanish = array(
     'somenergia:theme:slogan' => 'Donde el debate abierto, transparente y participativo tiene lugar.',
-    'somenergia:somheader:haztesocio' => 'Hazte socio',
+    'somenergia:somheader:haztesocio' => 'Hazte socio/a',
     'somenergia:somheader:contrata' => 'Contrata la luz',
     'somenergia:somheader:estasaqui' => 'Estás aquí: Plataforma',
     'somenergia:somheader:Web' => 'Web',
