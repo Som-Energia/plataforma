ac3f0b00305a8b6ca4dc4b363424e8c19d79e563
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-08-02 11:20:47 +0200

    remove menu from engine and migrate languages

--- mod/somenergia-theme/languages/eu.php
+++ mod/somenergia-theme/languages/eu.php
@@ -7,6 +7,60 @@
 
 $strings = array(
     'somenergia:theme:slogan' => 'La cooperativa de consumo y producción de energia verde',
+    'menu:howwork'=> 'Eragiketa',
+'menu:howwork:welcome'=> 'Ongi Group',
+'menu:howwork:guide'=> 'Lanean hasteko gida',
+'menu:howwork:general'=> 'Talde General',
+'menu:howwork:about'=> 'Plataforma Taldearen On',
+'menu:howwork:local'=> 'Group Bertako Taldeen laguntza',
+
+'menu:topics'=> 'Gaia',
+'menu:topics:autoproduction'=> 'Auto Argitaratutako',
+'menu:topics:commercialization'=> 'Merchandising',
+'menu:topics:projects'=> 'Produkzioaren Proiektuak',
+'menu:topics:renewable'=> 'Mota Berriztagarria',
+'menu:topics:proposals'=> 'Zerbitzuak proposamenak',
+
+'menu:diffusion' =>'Emisio',
+'menu:diffusion:school' =>'September Eskola',
+'menu:diffusion:talks' =>'Hitzaldiak',
+'menu:diffusion:training' =>'Prestakuntza',
+'menu:diffusion:communication' =>'Komunikazioa',
+'menu:diffusion:compute' =>'informatika',
+'menu:diffusion:translate' =>'Itzulpen',
+'menu:diffusion:various' =>'Beste Batzuk',
+'menu:diffusion:legal' => 'Zuzenbide Talde',
+
+
+'menu:cooperative' => 'Kooperatiba',
+'menu:cooperative:council'=> 'Gobernu Kontseiluak',
+'menu:cooperative:technical'=> 'Crew',
+'menu:local'=> 'Bertako Talde',
+'menu:cooperative:organization'=> 'Antolakuntza',
+'menu:cooperative:commissions'=> 'Komisioak',
+
+'menu:local:andalucia' => 'Andalucía',
+'menu:local:aragon'=> 'Aragón',
+'menu:local:asturias'=> 'Asturias',
+'menu:local:cantabria'=> 'Cantabria',
+'menu:local:mancha'=> 'Castilla la Mancha',
+'menu:local:leon'=> 'Castilla y León',
+'menu:local:catalunya'=> 'Cataluña',
+'menu:local:euskadi'=> 'Euskadi',
+'menu:local:extremadura'=> 'Extremadura',
+'menu:local:galicia'=> 'Galicia',
+'menu:local:balears'=> 'Islas Baleares',
+'menu:local:canarias'=> 'Islas Canarias',
+'menu:local:rioja'=> 'La Rioja',
+'menu:local:madrid'=> 'Madrid',
+'menu:local:murcia'=> 'Murcia',
+'menu:local:navarra'=> 'Navarra',
+'menu:local:valencia'=> 'Valencia',
+
+
+
+'menu:community' => 'Komunitatea',
+
 );
 
 add_translation("eu", $strings);
