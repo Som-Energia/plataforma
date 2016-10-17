8920d4229c10b892483604b4260111034821811d
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2016-10-17 12:33:49 +0200

    doc processes

---
+++ mod/group_tools/languages/ca.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Elgg groups plugin language pack
+ *
+ * @package ElggGroups
+ */
+
+$catalan = array(
+	'group_tools:menu:docs' => "Documentació del Procés",
+
+	);
+
+add_translation("ca", $catalan);
