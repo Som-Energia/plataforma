3b995ef181537ee6c27a8e10df286a95f793e143
hectorespert <blackleg@users.noreply.github.com>
2016-10-30 18:31:52 +0100

    Create ca.php

---
+++ mod/au_subgroups/languages/ca.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Assemblies -- Translation strings for Catalan
+ *
+ * @package        Lorea
+ * @subpackage     Assemblies
+ *
+ * Copyright 2012-2013 Lorea Faeries <federation@lorea.org>
+ *
+ * This file is part of the Assemblies plugin for Elgg.
+ *
+ * Assemblies is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Assemblies is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+$language = array (
+  'assemblies' => 'Assemblees',
+  'assemblies:assembly' => 'Assemblea',
+  'item:object:assembly' => 'Assemblees',
+  'item:object:decision' => 'Punts de l\'ordre del dia',
+  'assemblies:none' => 'No hi ha assemblees',
+  'assemblies:assembly:none' => 'No hi ha assemblees',
+  'assemblies:none-previous' => 'No hi ha assemblees anteriors',
+  'assemblies:general_assembly' => 'Assemblea general',
+  'assemblies:title:all_assemblies' => 'Totes les assemblees del lloc',
+  'assembly:edit_general' => 'Editar predefinits',
+  'assembly:group' => 'Assemblees del grup',
+  'assemblies:group' => 'Assemblea del grup',
+  'assemblies:decision:group' => 'Minuts del grup',
+  'assemblies:enableassemblies' => 'Activar les assemblees del grup',
+  'assemblies:next' => 'Propera assemblea',
+  'assemblies:assembly_location' => 'Localització predeterminada',
+  'assemblies:periodicity' => 'Periodicitat',
+  'assemblies:chat' => 'URL del chat',
+  'assemblies:streaming' => 'URL de l\'streaming',
+  'assemblies:voip' => 'URL del VoIP',
+  'assemblies:location' => 'Localització predeterminada',
+  'assemblies:assembly:title' => 'Títol',
+  'assemblies:assembly:description' => 'Descripció',
+  'assemblies:assembly:date' => 'Data',
+  'assemblies:assembly:time' => 'Hora',
+  'assemblies:assembly:saved' => 'Assemblea guardada correctament',
+  'assemblies:assembly:location' => 'Localització',
+  'assemblies:assembly:category' => 'Seccions (separades per comes)',
+  'assemblies:assembly:access_id' => 'Accés',
+  'assemblies:assembly:children' => 'Ordre del dia',
+  'assemblies:assembly:owner' => 'Creat per %s',
+  'assemblies:assembly:section:all' => 'Tots',
+  'assemblies:assembly:section:previous' => 'Propers',
+  'assemblies:assembly:section:next' => 'Fets',
+  'decision:add' => 'Afegir punt a l\'ordre del dia',
+  'decision:edit' => 'Edita punt de l\'ordre del dia',
+  'assemblies:decision:title' => 'Títol',
+  'assemblies:decision:description' => 'Objectiu',
+  'assemblies:decision:date' => 'Data',
+  'assemblies:decision:access_id' => 'Accés',
+  'assemblies:decision:status' => 'Estat',
+  'assemblies:decision:tags' => 'Etiquetes',
+  'assemblies:decision:proposal' => 'Proposta',
+  'assemblies:decision:mode' => 'Classe',
+  'assemblies:decision:owner' => 'Proposat per %s',
+  'assemblies:decision:children' => 'Propostes per a aquesta decisió',
+  'assemblies:decision:child' => 'Proposta',
+  'assemblies:decision:none' => 'No hi ha decisions',
+  'assemblies:decision:category' => 'Secció',
+  'assemblies:decision:section:all' => 'Totes',
+  'assemblies:decision:permanent' => 'Permanent',
+  'assemblies:decision:conjunctural' => 'Cojuntural',
+  'assemblies:decision:new' => 'Nova',
+  'assemblies:decision:accepted' => 'Acceptada',
+  'assemblies:decision:discarded' => 'Descartada',
+  'assemblies:decision:delayed' => 'Posposada',
+  'assemblies:decision:section:new' => 'Nova',
+  'assemblies:decision:section:accepted' => 'Acceptada',
+  'assemblies:decision:section:discarded' => 'Descartada',
+  'assemblies:decision:section:delayed' => 'Posposada',
+  'assemblies:link:view' => 'Veure',
+  'assemblies:link:edit' => 'Edita',
+  'assemblies:info' => 'Informació',
+  'assemblies:info:when' => 'Quan',
+  'assemblies:info:where' => 'A on',
+  'assemblies:agenda' => 'Ordre del dia',
+  'assemblies:minutes' => 'Minuts',
+  'assemblies:link:view:tooltip' => 'Veure les assemblees programades actualment',
+  'assemblies:link:edit:tooltip' => 'Edita les assemblees programades actualment',
+  'assemblies:link:view:all:tooltip' => 'Veure totes les assemblees',
+  'crud:assembly:nochildren' => 'No hi ha cap punt creat per a aquesta assemblea',
+  'crud:assembly:addchild' => 'Afegir',
+  'assembly:add' => 'Afegir convocatòria d\'assemblea',
+  'assembly:edit' => 'Editar convocatòria d\'assemblea',
+  'assemblies:message:saved' => 'Convocatòria d\'assemblea desada.',
+  'assemblies:error:cannot_save' => 'No es pot desar la crida a l\'assemblea.',
+  'assemblies:error:cannot_write_to_container' => 'No tens permisos per desar la assemblea al grup.',
+  'assemblies:message:deleted_assembly' => 'Assemblea esborrada.',
+  'assemblies:error:cannot_delete_assembly' => 'No es pot esborrar l\'assemblea.',
+  'assemblies:error:assembly_not_found' => 'Assemblea no trobada.',
+  'assemblies:message:deleted_decision' => 'Punt de l\'ordre del dia esborrat.',
+  'assemblies:error:cannot_delete_decision' => 'No es pot esborrar el punt de l\'ordre del dia.',
+  'assemblies:error:decision_not_found' => 'Punt de l\'ordre del dia no trobat.',
+  'assemblies:error:missing:title' => 'Si us plau, dona-li un nom a l\'assemblea!',
+  'assemblies:error:cannot_edit' => 'Aquesta assemblea sembla no existir o potser no tens permisos per editar-la.',
+  'river:create:object:assembly' => '%s ha publicat una convocatòria per a l\'assemblea %s',
+  'river:create:object:decision' => '%s ha publicat el punt de l\'ordre del dia %s',
+  'river:edited:object:assembly' => '%s ha editat la convocatòria a l\'assemblea %s',
+  'river:edited:object:decision' => '%s ha editat el punt de l\'ordre del dia %s',
+  'river:comment:object:assembly' => '%s ha comentat a l\'assemblea %s',
+  'river:comment:object:decision' => '%s ha comentat al punt de l\'ordre del dia %s',
+  'assemblies:newcall' => 'Una nova convocatòria d\'assemblea',
+  'assemblies:notification' => '%s ha fet una nova convocatòria d\'assemblea.
+
+%s
+%s
+
+Mira i suggereix noves propostes a l\'assemblea:
+%s',
+  'assemblies:decision:link' => 'Enllaça la següent assemblea',
+  'assemblies:decision:linked' => 'Proposta enllaçada a la propera assemblea',
+  'assemblies:decision:cantlink' => 'No es pot enllaçar la proposta',
+  'assemblies:decision:nonext' => 'No hi ha assemblees pendents',
+  'assemblies:widget:description' => 'Mostra les últimes convocatòries d\'assemblea',
+  'assemblies:moreassemblies' => 'Més convocatòries d\'assemblees',
+  'assemblies:numbertodisplay' => 'Nombre de convocatòries a mostrar',
+  'assemblies:nocalls' => 'No hi ha cap convocatòria',
+  'assemblies:assembly:numbertodisplay' => 'Nombre màxim d\'assemblees a mostrar',
+);
+add_translation("ca", $language);
