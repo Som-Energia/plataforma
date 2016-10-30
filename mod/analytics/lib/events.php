7072629c008c53862f8c5bdf5d543ce4a9ba39fb
blackleg <hectorespertpardo@gmail.com>
2016-10-30 17:10:23 +0100

    Upgrade analytics

--- mod/analytics/lib/events.php
+++ mod/analytics/lib/events.php
@@ -1,19 +1,32 @@
 <?php
+/**
+ * All event handlers are bundled here
+ */
 
-	function analytics_event_tracker($event, $object_type, $object){
-		if(!empty($object) && $object instanceof ElggEntity){
-			if(elgg_get_plugin_setting("trackEvents", "analytics") == "yes"){
-				switch($object->type){
-					case "object":
-						analytics_track_event($object->getSubtype(), $event, $object->title);
-						break;
-					case "group":
-						analytics_track_event($object->type, $event, $object->name);
-						break;
-					case "user":
-						analytics_track_event($object->type, $event, $object->name);
-						break;
-				}
+/**
+ * Track certan events on ElggEntity's
+ *
+ * @param string     $event       the event
+ * @param string     $object_type the type of the ElggEntity
+ * @param ElggEntity $object      the entity for the event
+ *
+ * @return void
+ */
+function analytics_event_tracker($event, $object_type, $object) {
+	
+	if (!empty($object) && ($object instanceof ElggEntity)) {
+		if (elgg_get_plugin_setting("trackEvents", "analytics") == "yes") {
+			switch ($object->type) {
+				case "object":
+					analytics_track_event($object->getSubtype(), $event, $object->title);
+					break;
+				case "group":
+					analytics_track_event($object->type, $event, $object->name);
+					break;
+				case "user":
+					analytics_track_event($object->type, $event, $object->name);
+					break;
 			}
 		}
 	}
+}
--- mod/analytics/lib/functions.php
+++ mod/analytics/lib/functions.php
@@ -1,52 +1,70 @@
-<?php 
+<?php
+/**
+ * All helper functions are bundled here
+ */
 
-	function analytics_track_action($action){
-		$action_result = true;
+/**
+ * Track Elgg actions if the setting allows this
+ *
+ * @param string $action the name of the action
+ *
+ * @return bool
+ */
+function analytics_track_action($action) {
+	$action_result = true;
+	
+	if (elgg_trigger_plugin_hook("track_action", "analytics", array("action" => $action), true)) {
+		if (count_messages("error") > 0) {
+			$action_result = false;
+		}
 		
-		if(elgg_trigger_plugin_hook("track_action", "analytics", array("action" => $action), true)){
-			if(count_messages("error") > 0){
-				$action_result = false;
-			}
-			
-			if(!array_key_exists("analytics", $_SESSION)){
+		if (!array_key_exists("analytics", $_SESSION)) {
+			$_SESSION["analytics"] = array();
+		}
+		
+		if (!array_key_exists("actions", $_SESSION["analytics"])) {
+			$_SESSION["analytics"]["actions"] = array();
+		}
+		
+		$_SESSION["analytics"]["actions"][$action] = $action_result;
+	}
+	
+	return true;
+}
+
+/**
+ * Track Elgg events if the settings allows this
+ *
+ * @param string $category category of the event
+ * @param string $action   the action of the event
+ * @param string $label    optional label for tracking
+ *
+ * @return bool
+ */
+function analytics_track_event($category, $action, $label = "") {
+	
+	if (elgg_trigger_plugin_hook("track_event", "analytics", array("category" => $category, "action" => $action, "label" => $label), true)) {
+		if (!empty($category) && !empty($action)) {
+			if (!array_key_exists("analytics", $_SESSION)) {
 				$_SESSION["analytics"] = array();
 			}
 			
-			if(!array_key_exists("actions", $_SESSION["analytics"])){
-				$_SESSION["analytics"]["actions"] = array();
+			if (!array_key_exists("events", $_SESSION["analytics"])) {
+				$_SESSION["analytics"]["events"] = array();
 			}
 			
-			$_SESSION["analytics"]["actions"][$action] = $action_result;
-		}
-		
-		return true;
-	}
-
-	function analytics_track_event($category, $action, $label = ""){
-		
-		if(elgg_trigger_plugin_hook("track_event", "analytics", array("category" => $category, "action" => $action, "label" => $label), true)){
-			if(!empty($category) && !empty($action)){
-				if(!array_key_exists("analytics", $_SESSION)){
-					$_SESSION["analytics"] = array();
-				}
-				
-				if(!array_key_exists("events", $_SESSION["analytics"])){
-					$_SESSION["analytics"]["events"] = array();
-				}
-				
-				$t_event = array(
-					"category" => $category,
-					"action" => $action
-				);
-				
-				if(!empty($label)){
-					$t_event["label"] = $label;
-				}
-				
-				$_SESSION["analytics"]["events"][] = $t_event;
+			$t_event = array(
+				"category" => $category,
+				"action" => $action
+			);
+			
+			if (!empty($label)) {
+				$t_event["label"] = $label;
 			}
+			
+			$_SESSION["analytics"]["events"][] = $t_event;
 		}
-		
-		return true;
 	}
 	
+	return true;
+}
