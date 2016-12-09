efecb7017d7960800af0d093579860f938b087cc
blackleg <hectorespertpardo@gmail.com>
2016-12-09 23:31:56 +0100

    Update mod logrotate

--- mod/logrotate/languages/en.php
+++ mod/logrotate/languages/en.php
@@ -1,29 +1,17 @@
 <?php
-/**
- * Elgg log rotator language pack.
- *
- * @package ElggLogRotate
- */
-
-$english = array(
+return array(
 	'logrotate:period' => 'How often should the system log be archived?',
-
-	'logrotate:weekly' => 'Once a week',
-	'logrotate:monthly' => 'Once a month',
-	'logrotate:yearly' => 'Once a year',
 
 	'logrotate:logrotated' => "Log rotated\n",
 	'logrotate:lognotrotated' => "Error rotating log\n",
-	
+
 	'logrotate:delete' => 'Delete archived logs older than a',
 
 	'logrotate:week' => 'week',
 	'logrotate:month' => 'month',
 	'logrotate:year' => 'year',
 	'logrotate:never' => 'never',
-		
+
 	'logrotate:logdeleted' => "Log deleted\n",
 	'logrotate:lognotdeleted' => "No logs deleted\n",
 );
-
-add_translation("en", $english);
--- mod/logrotate/languages/es.php
+++ mod/logrotate/languages/es.php
@@ -1,28 +1,17 @@
 <?php
-/**
- * Elgg log rotator language pack.
- *
- * @package ElggLogRotate
- */
+return array(
+	'logrotate:period' => '&iquest;Qu&eacute; frecuencioa deseas para rotar los registros?',
 
-$spanish = array(
-	'logrotate:period' => 'Con qu&eacute; frecuencia debe archivarse el log del sistema?',
-
-	'logrotate:weekly' => 'Una vez a la semana',
-	'logrotate:monthly' => 'Una vez al mes',
-	'logrotate:yearly' => 'Una vez al a&ntilde;o',
-
-	'logrotate:logrotated' => "Registro archivado\n",
-	'logrotate:lognotrotated' => "Error al archivar el registro\n",
+	'logrotate:logrotated' => "Registro rotado\n",
+	'logrotate:lognotrotated' => "Error al rotar los registros\n",
 	
-	'logrotate:date' => 'Eliminar registros archivados anteriores a:',
+	'logrotate:delete' => 'Borrar los registros archivados anteriores a',
 
-	'logrotate:week' => 'semana',
-	'logrotate:month' => 'mes',
-	'logrotate:year' => 'a&ntilde;o',
+	'logrotate:week' => 'una semana',
+	'logrotate:month' => 'un mes',
+	'logrotate:year' => 'un año',
+	'logrotate:never' => 'nunca',
 		
-	'logrotate:logdeleted' => "Registro eliminado\n",
-	'logrotate:lognotdeleted' => "Error al eliminar el registro\n",
+	'logrotate:logdeleted' => "Registro borrado\n",
+	'logrotate:lognotdeleted' => "Ningún log fue eliminado",
 );
-
-add_translation("es", $spanish);
