21cc2bfe9858e7fbf5506ee8b7d3fc004dcd9c6b
blackleg <hectorespertpardo@gmail.com>
2016-12-09 22:59:09 +0100

    Initial prepare rm mods to 1.9.7

--- mod/rm_group_autosubscription/actions/groups/membership/join.php
+++ mod/rm_group_autosubscription/actions/groups/membership/join.php
@@ -10,9 +10,6 @@
  * 
  * @package ElggGroups
  */
-
-global $CONFIG;
-
 $user_guid = get_input('user_guid', elgg_get_logged_in_user_guid());
 $group_guid = get_input('group_guid');
 
@@ -25,72 +22,81 @@ elgg_set_ignore_access($ia);
 
 if (($user instanceof ElggUser) && ($group instanceof ElggGroup)) {
 
-	// join or request
-	$join = false;
-	if ($group->isPublicMembership() || $group->canEdit($user->guid)) {
-		// anyone can join public groups and admins can join any group
-		$join = true;
-	} else {
-		if (check_entity_relationship($group->guid, 'invited', $user->guid)) {
-			// user has invite to closed group
-			$join = true;
-		}
-	}
+    // join or request
+    $join = false;
+    if ($group->isPublicMembership() || $group->canEdit($user->guid)) {
+        // anyone can join public groups and admins can join any group
+        $join = true;
+    } else {
+        if (check_entity_relationship($group->guid, 'invited', $user->guid)) {
+            // user has invite to closed group
+            $join = true;
+        }
+    }
 
-	if ($join) {
-		if (groups_join_group($group, $user)) {
-		    
-			system_message(elgg_echo("groups:joined"));
-			
-			/**
-			 * @author  Rubén Martín <soy@rubenmartin.me>
-			 * @dated   09/24/2013
-			 */
-			//{{{
-			
- 			add_entity_relationship( $user->get( 'guid' ), 'notifyemail', $group->get( 'guid' ) );
-			
-			$rm_topics = elgg_get_entities( array(
-			    'container_guids' => $group_guid,
-			    'types' => 'object',
-			    'subtypes' => 'groupforumtopic'	    
-			));
+    if ($join) {
+        if (groups_join_group($group, $user)) {
 
-			if ( ! empty( $rm_topics ) )
-			    foreach( $rm_topics as $topic )
-				comment_tracker_subscribe( $user_guid, $topic->guid );
-		    
-			//}}}
-			
-			forward($group->getURL());
-			
-		} else {
-			register_error(elgg_echo("groups:cantjoin"));
-		}
-	} else {
-		add_entity_relationship($user->guid, 'membership_request', $group->guid);
+            system_message(elgg_echo("groups:joined"));
 
-		// Notify group owner
-		$url = "{$CONFIG->url}groups/requests/$group->guid";
-		$subject = elgg_echo('groups:request:subject', array(
-			$user->name,
-			$group->name,
-		));
-		$body = elgg_echo('groups:request:body', array(
-			$group->getOwnerEntity()->name,
-			$user->name,
-			$group->name,
-			$user->getURL(),
-			$url,
-		));
-		if (notify_user($group->owner_guid, $user->getGUID(), $subject, $body)) {
-			system_message(elgg_echo("groups:joinrequestmade"));
-		} else {
-			register_error(elgg_echo("groups:joinrequestnotmade"));
-		}
-	}
+            /**
+             * @author  Rubén Martín <soy@rubenmartin.me>
+             * @dated   09/24/2013
+             */
+            //{{{
+
+            add_entity_relationship($user->get('guid'), 'notifyemail', $group->get('guid'));
+
+            $rm_topics = elgg_get_entities(array(
+                'container_guids' => $group_guid,
+                'types' => 'object',
+                'subtypes' => 'groupforumtopic'
+            ));
+
+            if (!empty($rm_topics)) {
+                foreach ($rm_topics as $topic) {
+                    comment_tracker_subscribe($user_guid, $topic->guid);
+                }
+            }
+
+            //}}}
+
+            forward($group->getURL());
+        } else {
+            register_error(elgg_echo("groups:cantjoin"));
+        }
+    } else {
+        add_entity_relationship($user->guid, 'membership_request', $group->guid);
+
+        // Notify group owner
+        $config_url = elgg_get_config('url');
+        $url = "{$config_url}groups/requests/$group->guid";
+        $subject = elgg_echo('groups:request:subject', array(
+            $user->name,
+            $group->name,
+        ));
+        $body = elgg_echo('groups:request:body', array(
+            $group->getOwnerEntity()->name,
+            $user->name,
+            $group->name,
+            $user->getURL(),
+            $url,
+        ));
+
+        $params = array(
+            'object' => NULL,
+            'action' => 'create',
+            'summary' => NULL,
+        );
+
+        if (notify_user($group->owner_guid, $user->getGUID(), $subject, $body, $params)) {
+            system_message(elgg_echo("groups:joinrequestmade"));
+        } else {
+            register_error(elgg_echo("groups:joinrequestnotmade"));
+        }
+    }
 } else {
-	register_error(elgg_echo("groups:cantjoin"));
+    register_error(elgg_echo("groups:cantjoin"));
 }
 
 forward(REFERER);
---
+++ mod/rm_group_autosubscription/languages/eu.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Pages languages
+ *
+ * @package SomEnergia
+ */
+
+$strings = array(
+    'menu:page:header:notifications'            => 'Notificaciones',
+    'rm_group_autosubscription:admin:button'    => 'Suscribir miembros a sus grupos',
+    'rm_group_autosubscription:email:reply'     => 'Nueva repuesta en el grupo',
+);
+
+add_translation("eu", $strings);
