04c2155f5cc3c46c239daf7ace0f60f7b3e8cfa8
blackleg <hectorespertpardo@gmail.com>
2016-12-11 00:24:28 +0100

    Add file tools translations

--- mod/somenergia_translations/languages/ca.php
+++ mod/somenergia_translations/languages/ca.php
@@ -7,6 +7,127 @@
  * @subpackage Languages.Spanish
  */
 $catalan = array(
+    'file_tools' => "Eines Fitxers",
+    'file_tools:file:actions' => 'Accions',
+    'file_tools:list:sort:type' => 'Tipus',
+    'file_tools:list:sort:time_created' => 'Data creaci�',
+    'file_tools:list:sort:asc' => 'Ascendent',
+    'file_tools:list:sort:desc' => 'Descendent',
+    // object name
+    'item:object:folder' => "Carpeta Fitxers",
+    // menu items
+    'file_tools:menu:mine' => "Teves Carpetes",
+    'file_tools:menu:user' => "%s's carpetes",
+    'file_tools:menu:group' => "Grup de carpetes de fitxers",
+    // group tool option
+    'file_tools:group_tool_option:structure_management' => "Permetre administraci� de carpetes per membres",
+    // views
+    // object
+    'file_tools:object:files' => "%s fitxer(s) en aquesta carpeta",
+    'file_tools:object:no_files' => "Cap fitxer en aquesta carpeta",
+    // input - folder select
+    'file_tools:input:folder_select:main' => "Carpeta Principal",
+    // list
+    'file_tools:list:title' => "Carpetes llista fitxers",
+    'file_tools:list:folder:main' => "Carpeta Principal",
+    'file_tools:list:files:none' => "Cap fitxer en aquesta carpeta",
+    'file_tools:list:select_all' => 'Selecciona tot',
+    'file_tools:list:deselect_all' => 'Deselecciona tot',
+    'file_tools:list:download_selected' => 'Descarregar seleccionat',
+    'file_tools:list:delete_selected' => 'Esborra seleccionat',
+    'file_tools:list:alert:not_all_deleted' => 'No tots els fitxers poden ser esborrats',
+    'file_tools:list:alert:none_selected' => 'Cap element seleccionat',
+    'file_tools:list:tree:info' => "Sabies que?",
+    'file_tools:list:tree:info:1' => "Pots arrossegar i deixar anar els fitxers a les carpetes per organitzar-les!",
+    'file_tools:list:tree:info:2' => "Pots fer doble click sobre qualsevol carpeta per expandir totes les seves sub-carpetes!",
+    'file_tools:list:tree:info:3' => "Pots reordenar carpetes arrossegant-les al seu nou lloc en el arbre!",
+    'file_tools:list:tree:info:4' => "Pots moure estructures completes de carpetes!",
+    'file_tools:list:tree:info:5' => "Si esborres una carpeta, pots escollir opcionalment esborrar tots els seus fitxers!",
+    'file_tools:list:tree:info:6' => "Quan esborres una carpeta, totes les subcarpetes seran esborrades!",
+    'file_tools:list:tree:info:7' => "Aquest missatge �s aleatori!",
+    'file_tools:list:tree:info:8' => "Quan esborres una carpeta, per� els seus fitxers, els fitxers
+apareixeran a la carpeta de nivel superior!",
+    'file_tools:list:tree:info:9' => "Una carpeta recentment afegida pot ser posada directament a la correcta subcarpeta!",
+    'file_tools:list:tree:info:10' => "Quan puges o edites un fitxer pots escollir en quina carpeta hauria d'apar�ixer!",
+    'file_tools:list:tree:info:11' => "Arrossegar fitxers �s �nicament disponible a la vista de llista, no a la  vista de galeria!",
+    'file_tools:list:tree:info:12' => "Pots actualitzar el nivell d'acces a totes les carpetes i fins i tot (opcional) de tots els fitxers durant l'edici� d'una carpeta!",
+    'file_tools:list:files:options:sort_title' => 'Ordenaci�',
+    'file_tools:list:files:options:view_title' => 'Vista',
+    'file_tools:usersettings:time' => 'Visualitzaci� Hora',
+    'file_tools:usersettings:time:description' => 'Canviar com es mostra la data de fitxer/carpeta ',
+    'file_tools:usersettings:time:default' => 'Visualitzaci� hora per defecte',
+    'file_tools:usersettings:time:date' => 'Data',
+    'file_tools:usersettings:time:days' => 'Dies enrere',
+    // new/edit
+    'file_tools:new:title' => "Nova carpeta fitxers",
+    'file_tools:edit:title' => "Editar carpeta fitxers",
+    'file_tools:forms:edit:title' => "T�tol",
+    'file_tools:forms:edit:description' => "Descripci�",
+    'file_tools:forms:edit:parent' => "Seleccionar una carpeta pare",
+    'file_tools:forms:edit:change_children_access' => "Actualitzar acc�s de totes les subcarpetes",
+    'file_tools:forms:edit:change_files_access' => "Actualitzar acces de tots els fitxers en aquesta carpeta (i totes les seves subcarpetes si es selecciona)",
+    'file_tools:forms:browse' => 'Navegar..',
+    'file_tools:forms:empty_queue' => 'Cua buida',
+    'file_tools:folder:delete:confirm_files' => "Vols tamb� eliminar tots els arxius en les (sub)carpetes a esborrar",
+    // actions
+    // edit
+    'file_tools:action:edit:error:input' => "Entrada incorrecte al crear/editar una carpeta",
+    'file_tools:action:edit:error:owner' => "No es pot trobar el propietari de la carpeta",
+    'file_tools:action:edit:error:folder' => "Cap carpeta a crear/editar",
+    'file_tools:action:edit:error:save' => "Error desconegut al guardar la carpeta",
+    'file_tools:action:edit:success' => "Carpeta creada/editada satisfact�riament",
+    'file_tools:action:move:parent_error' => "No es pot arrosseguar carpeta en si mateixa.",
+    // delete
+    'file_tools:actions:delete:error:input' => "Entrada incorrecte al esborrar una carpeta",
+    'file_tools:actions:delete:error:entity' => "El GUID no pot ser trobat",
+    'file_tools:actions:delete:error:subtype' => "El GUID no �s una carpeta",
+    'file_tools:actions:delete:error:delete' => "Error desconegut al eliminar la carpeta",
+    'file_tools:actions:delete:success' => "La carpeta s'ha esborrat satisfact�riament",
+    'file_tools:upload:new' => 'Pujar fitxer zip',
+    'file_tools:upload:form:choose' => 'Escollir fitxer',
+    'file_tools:upload:form:info' => 'Clicar navegar per pujar (multiple) fitxers',
+    'file_tools:upload:form:zip:info' => "Pots pujar un fitxer zip. S'extreur� i cada un dels fitxers  seran importats separadament. Tamb� si tens carpetes en el teu zip s'importaran en la corresponent carpeta. Tipus de fitxers que no estan permesos seran omesos.",
+    //errors
+    'file_tools:error:pageowner' => 'Error recuperant propietari p�gina.',
+    'file_tools:error:nofilesextracted' => 'No s\'ha trobat fitxers per extreure.',
+    'file_tools:error:cantopenfile' => 'Fitxer Zip no pot ser obert (revisa si el fitxer pujat �s un fitxar .zip).',
+    'file_tools:error:nozipfilefound' => 'Fitxer pujat no �s un fitxer .zip.',
+    'file_tools:error:nofilefound' => 'Escull un fitxer a pujar.',
+    //messages
+    'file_tools:error:fileuploadsuccess' => 'Fitxer Zip pujat i extret satisfact�riament.',
+    // move
+    'file_tools:action:move:success:file' => "El fitxer s'ha mogut satisfat�riament",
+    'file_tools:action:move:success:folder' => "La carpeta s'ha mogut satisfat�riament",
+    // buld delete
+    'file_tools:action:bulk_delete:success:files' => "Esborrats %s fitxers satisfat�riament",
+    'file_tools:action:bulk_delete:error:files' => "Hi ha hagut un error en l'esborrat d'alguns fitxers",
+    'file_tools:action:bulk_delete:success:folders' => "Esborrades %s carpetes satisfat�riament",
+    'file_tools:action:bulk_delete:error:folders' => "Hi ha hagut un error en l'esborrat d'algunes carpetes",
+    // reorder
+    'file_tools:action:folder:reorder:success' => "Reordenat la(les) carpetes satisfat�riament",
+    //settings
+    'file_tools:settings:allowed_extensions' => 'Extensions permeses (separades per comes)',
+    'file_tools:settings:user_folder_structure' => 'Usar estructura carpetes',
+    'file_tools:settings:sort:default' => 'Opcions ordenaci� carpetes per defecte',
+    'file:type:application' => 'Applicaci�',
+    'file:type:text' => 'Text',
+    // widgets
+    // file tree
+    'widgets:file_tree:title' => "Carpetes",
+    'widgets:file_tree:description' => "Mostrar les teves carpetes",
+    'widgets:file_tree:edit:select' => "Seleccionar quina(es) carpeta(es) mostrar",
+    'widgets:file_tree:edit:show_content' => "Mostrar el contingut de la(les) carpeta(es)",
+    'widgets:file_tree:no_folders' => "Cap carpeta configurada",
+    'widgets:file_tree:no_files' => "Cap fitxer configurat",
+    'widgets:file_tree:more' => "M�s carpetes",
+    'widget:file:edit:show_only_featured' => 'Mostrar nom�s els fitxers que apareixen',
+    'widget:file_tools:show_file' => 'Feature file (widget)',
+    'widget:file_tools:hide_file' => 'Unfeature file',
+    'widgets:file_tools:more_files' => 'M�s fitxers',
+    // Group files
+    'widgets:group_files:description' => "Mostrar els �ltims grups fitxers",
+    // index_file
+    'widgets:index_file:description' => "Mostrar els �ltims fitxers de la comunitat",
     'comments' => "Comentaris",
     'comment:notification:settings' => 'Notificacions de comentaris',
     'comment:notification:settings:description' => "Avisa'm quan hi hagi comentaris en elements als que estic subscrit.",
--- mod/somenergia_translations/languages/eu.php
+++ mod/somenergia_translations/languages/eu.php
@@ -1,6 +1,126 @@
 <?php
 
 $basque = array(
+    'file_tools' => "Fitxategi Tresnak",
+    'file_tools:file:actions' => "Ekintzak",
+    'file_tools:list:sort:type' => "Mota",
+    'file_tools:list:sort:time_created' => "Sortze data",
+    'file_tools:list:sort:asc' => "Behitik gora",
+    'file_tools:list:sort:desc' => "Goitik behera",
+    // object name
+    'item:object:folder' => "Fitxategi Karpeta",
+    // menu items
+    'file_tools:menu:mine' => "Zure karpetak",
+    'file_tools:menu:user' => "%s(r)en karpetak",
+    'file_tools:menu:group' => "Taldeko fitxategi karpetak",
+    // group tool option
+    'file_tools:group_tool_option:structure_management' => "Baimena eman kideei karpetak kudeatzeko",
+    // views
+    // object
+    'file_tools:object:files' => "%s fitxategi karpeta honetan",
+    'file_tools:object:no_files' => "Ez dago fitxategirik karpeta honetan",
+    // input - folder select
+    'file_tools:input:folder_select:main' => "Karpeta nagusia",
+    // list
+    'file_tools:list:title' => "Fitxategi karpetak zerrendatu",
+    'file_tools:list:folder:main' => "Karpeta nagusia",
+    'file_tools:list:files:none' => "Ez da fitxategirik aurkitu karpeta honetan",
+    'file_tools:list:select_all' => "Hautatu dena",
+    'file_tools:list:deselect_all' => "Alderantzikatu hautapena",
+    'file_tools:list:download_selected' => "Deskargatu hautapena",
+    'file_tools:list:delete_selected' => "Ezabatu hautapena",
+    'file_tools:list:alert:not_all_deleted' => "Ezin izan da fitxategi guztiak ezabatu",
+    'file_tools:list:alert:none_selected' => "Ez dago ezer hautatua",
+    'file_tools:list:tree:info' => "Bazenekien?",
+    'file_tools:list:tree:info:1' => "Fitxategiak eramaten eta uzten ahal dituzu karpetetan, beraiek antolatzeko!",
+    'file_tools:list:tree:info:2' => "Bi aldiz klikatzen ahal duzu karpeten gainean, bere azpi-karpeta guztiak zabaltzeko!",
+    'file_tools:list:tree:info:3' => "Karpetak berrantola ditzakezu eramaten badituzu bere zuhaitzeko leku berrira!",
+    'file_tools:list:tree:info:4' => "Karpeta egitura osoak mugi ditzakezu!",
+    'file_tools:list:tree:info:5' => "Karpeta bat ezabatzen baduzu, aukera dezakezu fitxategi guztiak ezabatzea!",
+    'file_tools:list:tree:info:6' => "Karpeta bat ezabatzean, azpi-karpeta guztiak ere ezabatzen ari zara!",
+    'file_tools:list:tree:info:7' => "Mezu hau ausazkoa da!",
+    'file_tools:list:tree:info:8' => "Karpeta bat ezabatzean, baina ez bere fitxategiak, horiek goi mailako karpetan ageriko dira!",
+    'file_tools:list:tree:info:9' => "Berriki gaineratutako karpeta, azpi-karpeta egokian jar daiteke zuzenean!",
+    'file_tools:list:tree:info:10' => "Fitxategi bat igotzean edo editatzean, zein karpetatan agertzea nahi duzun aukera dezakezu!",
+    'file_tools:list:tree:info:11' => "Fitxategiak eramatea egin daiteke zerrenda bistan soilik, ez galeria bistan!",
+    'file_tools:list:tree:info:12' => "Karpeta bat editatzean, sartze-maila eguneratzen ahal duzu azpi-karpeta guztietan edota fitxategi guztietan!",
+    'file_tools:list:files:options:sort_title' => " Antolatu",
+    'file_tools:list:files:options:view_title' => "Ikusi",
+    'file_tools:usersettings:time' => "Denbora ikustea",
+    'file_tools:usersettings:time:description' => "Aldatu fitxategiaren/karpetaren denbora ikusten den modua",
+    'file_tools:usersettings:time:default' => "Denbora ikuste lehenetsia",
+    'file_tools:usersettings:time:date' => "Data",
+    'file_tools:usersettings:time:days' => "Duela zenbat egun",
+    // new/edit
+    'file_tools:new:title' => "Fitxategi karpeta  berria",
+    'file_tools:edit:title' => "Fitxategi karpeta editatu",
+    'file_tools:forms:edit:title' => "Izenburua",
+    'file_tools:forms:edit:description' => "Deskripzioa",
+    'file_tools:forms:edit:parent' => "Karpeta nagusi bat hautatu",
+    'file_tools:forms:edit:change_children_access' => "Azpi-karpeta guztietan sartzea eguneratu",
+    'file_tools:forms:edit:change_files_access' => "Fitxategi guztietan sartzea eguneratu, karpeta honetan (eta azpi-karpeta guztietan, hautatuz gero)",
+    'file_tools:forms:browse' => "Begiratu...",
+    'file_tools:forms:empty_queue' => "Ilara hustu",
+    'file_tools:folder:delete:confirm_files' => "Kendutako (azpi)karpetetako fitxategi guztiak ere ezabatu nahi al dituzu?",
+    // actions
+    // edit
+    'file_tools:action:edit:error:input' => "Sarrera desegokia da fitxategi karpeta bat sortzeko/editatzeko",
+    'file_tools:action:edit:error:owner' => "Ezin izan da fitxategi karpetaren jabea aurkitu",
+    'file_tools:action:edit:error:folder' => "Ez dago sortzeko/editatzeko karpetarik",
+    'file_tools:action:edit:error:save' => "Errore ezezaguna gertatu da fitxategi karpeta gordetzean",
+    'file_tools:action:edit:success' => "Fitxategi karpeta egoki sortu/editatu da",
+    'file_tools:action:move:parent_error' => "Ezin da karpeta bere baitan utzi.",
+    // delete
+    'file_tools:actions:delete:error:input' => "Sarrera ez da balekoa fitxategi karpeta ezabatzeko",
+    'file_tools:actions:delete:error:entity' => "Emandako GUID ezin izan da aurkitu",
+    'file_tools:actions:delete:error:subtype' => "Emandako GUID ez da fitxategi karpeta bat",
+    'file_tools:actions:delete:error:delete' => "Errore ezezaguna gertatu da fitxategi karpeta ezabatzean",
+    'file_tools:actions:delete:success' => "Fitxategi karpeta egoki ezabatu da",
+    'file_tools:upload:new' => "Igo zip fitxategia",
+    'file_tools:upload:form:choose' => "Aukeratu fitxategia",
+    'file_tools:upload:form:info' => "Sakatu Begiratu fitxategiak igotzeko",
+    'file_tools:upload:form:zip:info' => "Zip fitxategi bat igo dezakezu. Hori erauzia izango da eta fitxategiak banaka inportatuak izango dira. Zure zip horretan fitxategiak badituzu, horiek ere inportatuak izango dira, dagokien karpetan. Fitxategi mota ez onartuak alde batera utziko dira.",
+    //errors
+    'file_tools:error:pageowner' => "Errorea orrialdeko jabea berreskuratzean.",
+    'file_tools:error:nofilesextracted' => "Ez da aurkitu onartutako fitxategirik erauzteko.",
+    'file_tools:error:cantopenfile' => "Zip fitxategia ezin izan da ireki (begiratu ea igotako fitxategia .zip fitxategi bat den).",
+    'file_tools:error:nozipfilefound' => "Igotako fitxategia ez da .zip fitxategi bat.",
+    'file_tools:error:nofilefound' => "Aukeratu fitxategi bat igotzeko.",
+    //messages
+    'file_tools:error:fileuploadsuccess' => "Zip fitxategia egoki igoa eta erauzia izan da.",
+    // move
+    'file_tools:action:move:success:file' => "Fitxategia egoki mugitua izan da",
+    'file_tools:action:move:success:folder' => "Karpeta egoki mugitua izan da",
+    // buld delete
+    'file_tools:action:bulk_delete:success:files' => "%s fitxategi egoki ezabatuak",
+    'file_tools:action:bulk_delete:error:files' => "Errorea gertatu da fitxategi batzuk ezabatzean",
+    'file_tools:action:bulk_delete:success:folders' => "%s karpeta egoki ezabatuak",
+    'file_tools:action:bulk_delete:error:folders' => "Errorea gertatu da karpeta batzuk ezabatzean",
+    // reorder
+    'file_tools:action:folder:reorder:success' => "Karpeta(k) egoki berrantolatua(k)",
+    //settings
+    'file_tools:settings:allowed_extensions' => "Luzapen onartuak (komen bidez bereiztuta)",
+    'file_tools:settings:user_folder_structure' => "Erabili karpetaren egitura",
+    'file_tools:settings:sort:default' => "Karpeta sortzeko aukera lehenetsiak",
+    'file:type:application' => "Aplikazioa",
+    'file:type:text' => "Testua",
+    // widgets
+    // file tree
+    'widgets:file_tree:title' => "Karpetak",
+    'widgets:file_tree:description' => "Erakutsi zure fitxategi karpetak",
+    'widgets:file_tree:edit:select' => "Hautatu agertuko d(ir)en karpeta(k)",
+    'widgets:file_tree:edit:show_content' => "Erakutsi karpet(ar)en edukia",
+    'widgets:file_tree:no_folders' => "Ez dago prestatutako karpetarik",
+    'widgets:file_tree:no_files' => "Ez dago prestatutako fitxategirik",
+    'widgets:file_tree:more' => "Fitxategi karpeta gehiago",
+    'widget:file:edit:show_only_featured' => "Erakutsi nabarmendutako fitxategiak soilik",
+    'widget:file_tools:show_file' => "Nabarmendu fitxategia (widget)",
+    'widget:file_tools:hide_file' => "Ez nabarmendu fitxategia",
+    'widgets:file_tools:more_files' => "Fitxategi gehiago",
+    // Group files
+    'widgets:group_files:description' => "Erakutsi azken taldearen fitxategiak",
+    // index_file
+    'widgets:index_file:description' => "Erakutsi azken fitxategiak zure elkartean",
     'comments' => 'Iruzkinak',
     'comment:notification:settings' => 'Iruzkinen abisuak',
     'comment:notification:settings:description' => 'Esan noiz iruzkinak argitaratzen diren zuk harpidetu dituzun gaietan.',
