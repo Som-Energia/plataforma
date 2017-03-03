3a3787b6709c31257685472c8313334719056b72
blackleg <hectorespertpardo@gmail.com>
2017-03-03 17:28:32 +0100

    Fix deprecated methods in somenergia index

--- mod/somenergia_index/start.php
+++ mod/somenergia_index/start.php
@@ -9,9 +9,9 @@ elgg_register_event_handler('init', 'system', 'somenergia_index_init');
 function somenergia_index_init() {
 	// Extend system CSS with our own styles
 	elgg_extend_view('css/elgg', 'somenergia_index/css');
-
-	// Replace the default index page
-	elgg_register_plugin_hook_handler('index', 'system', 'somenergia_index');
+        
+        // Replace the default index page
+	elgg_register_page_handler('', 'somenergia_index');
 }
 
 function somenergia_index($hook, $type, $return, $params) {
