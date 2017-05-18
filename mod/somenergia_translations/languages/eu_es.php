ca71bfcc649a2a609cd9480d4d61bfcd61c384ab
blackleg <hectorespertpardo@gmail.com>
2017-05-18 22:33:08 +0200

    Update comment tracker Euskera translation

--- mod/somenergia_translations/languages/eu_es.php
+++ mod/somenergia_translations/languages/eu_es.php
@@ -582,13 +582,15 @@ Argitaratze osoa ikusi ahal izateko, klikatu hurrengo estekan:
     'widgets:group_files:description' => "Erakutsi azken taldearen fitxategiak",
     // index_file
     'widgets:index_file:description' => "Erakutsi azken fitxategiak zure elkartean",
-    'comments' => 'Iruzkinak',
+    /* Comment Tracker Translation */
+    'comments' => "Iruzkinak",
     'comment:notification:settings' => 'Iruzkinen abisuak',
     'comment:notification:settings:description' => 'Esan noiz iruzkinak argitaratzen diren zuk harpidetu dituzun gaietan.',
     'comment:notification:settings:how' => 'Aukera ezazu zure komunikazio-era',
     'comment:notification:settings:linktext' => 'Ikus itzazu harpidetu egin dituzun gai guztiak',
     'comment:subscriptions' => 'Harpidetzak',
     'comment:subscriptions:none' => 'Ez dago jarduneko harpidetzarik',
+    'comment_tracker:setting:notify_owner' => "¿Permitir que el rastreador maneje las notificaciones de los propietarios de los comentarios?",
     'comment:subscribe:tooltip' => 'Harpidetu jakinarazpenak jasotzeko gai honi buruzko iruzkinak eginez gero',
     'allow:comment:notification' => 'Jakinarazpenak aktibatu nahi al dituzu? ',
     'email:content:type' => 'Jaso nahi al duzu emailak HTML formatuan? ',
@@ -598,116 +600,36 @@ Argitaratze osoa ikusi ahal izateko, klikatu hurrengo estekan:
     'comment:unsubscribe' => 'Harpidetza indargabetu',
     'comment:subscribe:long' => 'Harpidetu jakinarazpenak egiteko',
     'comment:unsubscribe:long' => 'Indargabetu jakinarazpenak egiteko',
-    'comment_tracker:setting:autosubscribe' => 'Auto-harpidetza nik iruzkindu dudan edukira?',
+    'comment_tracker:setting:autosubscribe' => "Auto-harpidetza nik iruzkindu dudan edukira?",
     'show:subscribers' => 'Erakutsi harpidedunak',
     'comment:subscribe:success' => 'Eduki edota gai honetara arrakastatsu harpidetu egin duzu.',
-    'comment:subscribe:failed' => 'Barkatu! Ezin duzu eduki edota gai honetara harpidetu.',
-    'comment:subscribe:entity:not:access' => 'Barkatu! Ezin dugu aurkitu edukia edota gaia arrazoiren batengatik.',
+    'comment:subscribe:failed' => "Barkatu! Ezin duzu eduki edota gai honetara harpidetu.",
+    'comment:subscribe:entity:not:access' => "Barkatu! Ezin dugu aurkitu edukia edota gaia arrazoiren batengatik.",
     'comment:unsubscribe:success' => 'Eduki edota gai honetatik arrakastatsu indargabetu egin duzu.',
-    'comment:unsubscribe:failed' => 'Barkatu! Ezin duzu eduki edota gai honetatik indargabetu.',
+    'comment:unsubscribe:failed' => "Barkatu! Ezin duzu eduki edota gai honetatik indargabetu.",
     'comment:unsubscribe:not:valid:url' => 'Barkatu! Hau ez da baliozko URLa eduki edota gai honetatik indargabetzeko.',
-    'comment:unsubscribe:entity:not:access' => 'Barkatu! Ezin dugu edukia edota gaia aurkitu.',
-    'comment_tracker:setting:show_button' => 'Erakutsi harpidetu/indargabetu botoia iruzkinen gainetik?',
-    'comment_tracker:item' => 'item',
-    'comment:notify:subject:groupforumtopic' => '%s eztabaidari erantsirik %s taldean %s',
-    'comment:notify:subject:comment' => '%s iruzkindurik  %s "%s"',
-    'comment:notify:subject:comment:group' => '%s iruzkindurik %s "%s" taldean %s',
-    /* Legacy stuff - @TODO - see what's safe to delete */
-    'comment:notify:subject' => 'Iruzkin berri bat badago %s',
-    'comment:notify:group:subject' => 'Iruzkin berri bat badago eztabaidan %s',
-    'comment:notify:body:web' => 'Kaixo %s,
-<br/>Iruzkin berri bat badago %s
-<br/>%s idatzi zuen: %s
-<br/>%s
-<br/>
-<font color="#888888" size="2">
-Jakinarazpen hau jaso egin duzu eduki edota gai honetara harpideturik edo tartean sarturik zaudelako.<br/>
-Zure jakinarazpenen lehentasunak aldatzeko, mesedez klikatu hemen: %s<br/>
-</font>
-        ',
-    'comment:notify:group:body:web' => 'Kaixo %s,
-<br/>Iruzkin berri bat badago eztabaidan %s
-<br/>%s idatzi zuen: %s
-<br/>%s
-<br/>
-<font color="#888888" size="2">
-Jakinarazpen hau jaso egin duzu eduki edota gai honetara harpideturik edo tartean sarturik zaudelako.<br/>
-Zure jakinarazpenen lehentasunak aldatzeko, mesedez klikatu hemen: %s<br/>
-</font>
-        ',
-    'comment:notify:body:email:text' => 'Hi %s,
+    'comment:unsubscribe:entity:not:access' => "Barkatu! Ezin dugu edukia edota gaia aurkitu.",
+    'comment_tracker:setting:show_button' => "Erakutsi harpidetu/indargabetu botoia iruzkinen gainetik?",
+    'comment_tracker:setting:show_entity_button' => "¿Mostrar el botón de suscripción/cancelación arriba en los menús de los componentes?",
+    'comment_tracker:setting:show_river_button' => "¿Mostrar el botón de suscripción/cancelación arriba en los menús de los elementos del River?",
+    'comment_tracker:item' => "item",
+    'comment_tracker:notify:subject' => '%s iruzkindurik  %s "%s"',
+    'comment_tracker:notify:subject:group' => '%s iruzkindurik %s "%s" taldean %s',
+    'comment_tracker:notify:body' => "Kaixo %s
 
-Iruzkin berri bat badago %s
+Hay un nuevo comentario en \"%s\".
 
-%s idatzi zuen: %s
+%s escribió:
+\"%s\"
 
+You can reply to the post here:
 %s
 
-Goraintziak,
+
+--
+Has recibido esta notificación porque te has suscrito para recibir notificaciones sobre nuevos comentarios. Para cambiar las preferencias de notificación de este tipo de mensajes, haz clic aquí:
 %s
-
-_________________________________________________________________________________
-<font color="#888888" size="2">
-Adi egon, zure edukia edota gaia ikusi baino lehen sisteman sar zenezake.
-
-Jakinarazpen hau jaso egin duzu eduki edota gai honetara harpideturik edo tartean sarturik zaudelako.
-
-Zure jakinarazpenen lehentasunak aldatzeko, mesedez klikatu hemen: %s
-</font>
-        ',
-    'comment:notify:group:body:email:text' => 'Kaixo %s,
-
-Iruzkin berri bat badago eztabaidan %s
-
-%s idatzi zuen: %s
-
-%s
-
-Goraintziak,
-%s
-
-_________________________________________________________________________________
-<font color="#888888" size="2">
-Adi egon, zure edukia edota gaia ikusi baino lehen sisteman sar zenezake.
-
-Jakinarazpen hau jaso egin duzu eduki edota gai honetara harpideturik edo tartean sarturik zaudelako.
-
-Zure jakinarazpenen lehentasunak aldatzeko, mesedez klikatu hemen: %s
-</font>
-        ',
-    'comment:notify:body:email:html' => '
-<div>
-    <div>Kaixo %s,</div>
-    <div>Iruzkin berri bat badago %s</div>
-    <div>%s idatzi zuen: %s</div>
-    <div>%s</div>
-    <div>&nbsp;</div>
-    <div>Goraintziak,</div></div>
-    <div>%s</div>
-    <div>&nbsp;</div>
-    <div style="border-top:1px solid #CCCCCC;color:#888888;">
-        <div>Adi egon, zure edukia edota gaia ikusi baino lehen sisteman sar zenezake.</div>
-        <div>Jakinarazpen hau jaso egin duzu eduki edota gai honetara harpideturik edo tartean sarturik zaudelako.</div>
-        <div>Zure jakinarazpenen lehentasunak aldatzeko, mesedez klikatu hemen: %s</div>
-    </div>
-</div>
-        ',
-    'comment:notify:group:body:email:html' => '
-<div>
-    <div>Kaixo %s,</div>
-    <div>Iruzkin berri bat badago eztabaidan %s</div>
-    <div>%s idatzi zuen: %s</div>
-    <div>%s</div>
-    <div>&nbsp;</div>
-    <div>Goraintziak,</div>
-    <div>%s</div>
-    <div style="border-top:1px solid #CCCCCC;color:#888888;">
-        <div>Adi egon, zure edukia edota gaia ikusi baino lehen sisteman sar zenezake.</div>
-        <div>Jakinarazpen hau jaso egin duzu eduki edota gai honetara harpideturik edo tartean sarturik zaudelako.</div>
-        <div>Zure jakinarazpenen lehentasunak aldatzeko, mesedez klikatu hemen: %s</div>
-    </div>
-</div>
-        ',
+",
     'au_subgroups' => "Subgrupos",
     'au_subgroups:subgroup' => "Subgrupo",
     'au_subgroups:subgroups' => "Subgrupos",
